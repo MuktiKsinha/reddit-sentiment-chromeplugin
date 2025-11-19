9e756bca3d45c6a0827d0c0725d5ea8d50838a2a
Muktiksinha <muktikumarsinha@gmail.com>
2025-11-19 23:01:34 +0530

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A small chrome plugin to detect reddit comment sentiments',
+    author='mukti kumar sinha',
+    license='',
+)
